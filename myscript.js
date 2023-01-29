4aa5f34c9d383ee664cdef17fcc8387bee17c455
technero <nermeen.hamed86@gmail.com>
2023-01-29 00:49:10 -0800

    add bar and doughnut chart

--- myscript.js
+++ myscript.js
@@ -12,7 +12,7 @@ alertBanner.getElementsByClassName("alert-banner-close")[0].addEventListener('cl
 })
 
 
-const trafficCanvas = document.getElementById("trafficChart").getContext("2d");
+const trafficCanvas = document.getElementById("traffic-chart").getContext("2d");
 
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
@@ -50,3 +50,68 @@ let trafficChart = new Chart(trafficCanvas, {
     options: trafficOptions
 });
 
+const dailyCanvas = document.getElementById("daily-chart").getContext("2d");
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+const mobileCanvas = document.getElementById("mobile-chart").getContext("2d");
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8']
+    }]
+};
+
+const mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
