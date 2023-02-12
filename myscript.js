bb63d2dd776532408581445779f323d72775d741
technero <nermeen.hamed86@gmail.com>
2023-02-12 12:46:07 -0800

    make responsive traffic chart

--- myscript.js
+++ myscript.js
@@ -12,20 +12,24 @@ alertBanner.getElementsByClassName("alert-banner-close")[0].addEventListener('cl
 })
 
 
-const trafficCanvas = document.getElementById("traffic-chart").getContext("2d");
+const notificationIcon = document.getElementById("bell");
+notificationIcon.addEventListener("click", () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+});
 
-let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
-        "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-            2500],
-        backgroundColor: 'rgba(116, 119, 191, .3)',
-        borderWidth: 1,
-    }]
+notificationIcon.addEventListener("blur", () => {
+    console.log("Here");
+    document.getElementById("myDropdown").classList.remove("show");
+})
+
+const updateChart = (chart, newData) => {
+    chart.data.labels = newData.labels;
+    chart.data.datasets[0].data = newData.datasets[0].data;
+    chart.update();
 };
 
-let trafficOptions = {
+const trafficCanvas = document.getElementById("traffic-chart").getContext("2d");
+const trafficOptions = {
     backgroundColor: 'rgb(112, 104, 201)',
     fill: true,
     aspectRatio: 2.5,
@@ -44,9 +48,64 @@ let trafficOptions = {
     }
 };
 
-let trafficChart = new Chart(trafficCanvas, {
+const trafficData = [
+    {
+        labels: ["12:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
+            "07:00", "08:00", "09:00", "10:00"],
+        datasets: [{
+            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+                2500],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    },
+    {
+        labels: ["1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7",
+            "1/8", "1/9", "1/10", "1/11"],
+        datasets: [{
+            data: [7500, 500, 8000, 11000, 3000, 17500, 9500, 9500, 22500, 30000,
+                25000],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    },
+    {
+        labels: ["1/1", "1/8", "1/15", "1/22", "1/29", "2/5", "2/12",
+            "2/19", "2/26", "3/5", "3/12"],
+        datasets: [{
+            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+                2500],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    },
+    {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+            "Aug", "Sep", "Oct", "Nov"],
+        datasets: [{
+            data: [7500, 500, 8000, 11000, 3000, 17500, 9500, 9500, 22500, 30000,
+                25000],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    },
+];
+
+const trafficIntervals = document.getElementsByClassName("traffic-nav-link");
+console.log(trafficIntervals);
+for (let i = 0; i < 4; i++) {
+    trafficIntervals[i].addEventListener("click", () => {
+        new Chart(trafficCanvas, {
+            type: 'line',
+            data: trafficData[i],
+            options: trafficOptions
+        });
+    });
+};
+
+new Chart(trafficCanvas, {
     type: 'line',
-    data: trafficData,
+    data: trafficData[0],
     options: trafficOptions
 });
 
@@ -123,3 +182,16 @@ let mobileChart = new Chart(mobileCanvas, {
 const user = document.getElementById("userField");
 const message = document.getElementById("messageField");
 const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out user and message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
+});
